df617a679ac53f20fd1076ca98892ee37e663c8d
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-08 09:40:28 +0200

    DirectoryHelper

---
+++ src/main/java/com/QYun/AssetReader4J/Helpers/ImportHelper.java
@@ -0,0 +1,13 @@
+package com.QYun.AssetReader4J.Helpers;
+
+import java.io.File;
+
+public class ImportHelper {
+    public static void mergeSplitAssets(File file) {
+        mergeSplitAssets(file, false);
+    }
+
+    public static void mergeSplitAssets(File file, boolean subDirectories) {
+
+    }
+}
