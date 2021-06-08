2bc54a241ebc82849b2e8f47be4f8a77f7c6d3b7
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-08 07:37:44 +0200

    App

---
+++ src/main/java/com/QYun/AssetReader4J/AssetsManager.java
@@ -0,0 +1,11 @@
+package com.QYun.AssetReader4J;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class AssetsManager {
+
+    public void LoadFiles(ArrayList<File> files) {
+
+    }
+}
