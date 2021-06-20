eb34afdfd26ce430610910f1d2c21cf3c47c4529
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-20 10:10:29 +0200

    toArray()为什么返回的不是T[]而是Object[]，这让Primitive咋整啊

--- src/main/java/com/QYun/AssetReader4J/BinaryReader.java
+++ src/main/java/com/QYun/AssetReader4J/BinaryReader.java
@@ -1,9 +1,12 @@
 package com.QYun.AssetReader4J;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class BinaryReader extends EndianInputStream {
     public BinaryReader(File file, boolean isLittleEndian) throws FileNotFoundException {
@@ -20,17 +23,32 @@ public class BinaryReader extends EndianInputStream {
             pos += alignment - mod;
     }
 
-    public String readAlignedString() {
-        try {
-            int length = readInt();
-            if (length > 0 && length <= fileLen - pos) {
-                var str = new String(readBytes(length), StandardCharsets.UTF_8);
-                alignStream();
-                return str;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public String readAlignedString() throws IOException {
+        int length = readInt();
+        if (length > 0 && length <= fileLen - pos) {
+            var str = new String(readBytes(length), StandardCharsets.UTF_8);
+            alignStream();
+            return str;
         }
         return "";
     }
+
+    public String readStringToNull() throws IOException {
+        return readStringToNull(32767);
+    }
+
+    public String readStringToNull(int maxLength) throws IOException {
+        ArrayList<byte> bytes = new ArrayList<byte>();
+        int i = 0;
+        while (pos != fileLen && i < maxLength) {
+            byte b = readByte();
+            if (b == 0)
+                break;
+            bytes.add(b);
+            i++;
+        }
+        return new String(ArrayUtils.toPrimitive((Byte[]) bytes.toArray()), StandardCharsets.UTF_8);
+    }
+
+
 }
