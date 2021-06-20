967426e00f55c97a21b96bb2930b74f235d48bd3
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-20 14:10:12 +0200

    isSerializedFile

--- src/main/java/com/QYun/AssetReader4J/SerializedFile.java
+++ src/main/java/com/QYun/AssetReader4J/SerializedFile.java
@@ -1,7 +1,38 @@
 package com.QYun.AssetReader4J;
 
+import java.io.IOException;
+
 public class SerializedFile {
     public static boolean isSerializedFile(BinaryReader reader) {
+        try {
+            if (reader.fileLen < 20)
+                return false;
 
+            int m_MetadataSize = reader.readInt();
+            long m_FileSize = reader.readInt();
+            int m_Version = reader.readInt();
+            long m_DataOffset = reader.readInt();
+            byte m_Endianess = reader.readByte();
+            byte[] m_Reserved = reader.readBytes(3);
+
+            if (m_Version >= 22) {
+                if (reader.fileLen < 48)
+                    return false;
+
+                m_MetadataSize = reader.readInt();
+                m_FileSize = reader.readLong();
+                m_DataOffset = reader.readLong();
+            }
+            if (m_FileSize != reader.fileLen || m_DataOffset > reader.fileLen) {
+                reader.setPos(0);
+                return false;
+            }
+
+            reader.setPos(0);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
